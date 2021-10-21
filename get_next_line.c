f121e3834112df00e575a1597557df6e5160611d
Leonardo Moreno <lmoreno@c1r8p7.42quebec.com>
2021-10-21 18:31:03 -0400

    Pasando Testor

--- get_next_line.c
+++ get_next_line.c
@@ -1,36 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: LeoMoreno <lmoreno@student.42quebec.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 17:41:14 by LeoMoreno         #+#    #+#             */
+/*   Updated: 2021/10/21 18:17:01 by LeoMoreno        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-
-char *read_line(int fd, char *str_join)
+char	*read_line(int fd, char *str_join)
 {
-	char *buff;
-	int bytes_lu;
+	char	*buff;
+	int		bytes_lu;
 
-	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	//if (buff == NULL)
-	//	return (NULL);
+	buff = malloc(sizeof(*buff) * BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
 	bytes_lu = 1;
 	while (!ft_strchr(str_join, '\n') && bytes_lu != 0)
-	//while (bytes_lu != 0)
 	{
 		bytes_lu = read(fd, buff, BUFFER_SIZE);
+		if (bytes_lu == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
 		buff[bytes_lu] = '\0';
-		printf("size_buff = %d, buff = %s\n", bytes_lu, buff);
 		str_join = ft_strjoin(str_join, buff);
 	}
 	free (buff);
 	return (str_join);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *line_lu;
-	char *line;
-
+	static char	*line_lu;
+	char		*line;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);	
 	line_lu = read_line(fd, line_lu);
+	if (!line_lu)
+		return (NULL);
 	line = ft_get_line(line_lu);
 	line_lu = ft_save_str(line_lu);
-	printf("\nLineLU: %s \n\n", line_lu);
-
 	return (line);
 }
